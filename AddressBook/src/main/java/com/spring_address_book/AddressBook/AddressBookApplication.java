a0e556c73db2e71371cecff7469cefd0d37c0444
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-03-07 15:25:30 +0530

    [Naincy] add AddressBookApp Creating a Address Book Spring Project

---
+++ AddressBook/src/main/java/com/spring_address_book/AddressBook/AddressBookApplication.java
@@ -0,0 +1,13 @@
+package com.spring_address_book.AddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApplication.class, args);
+	}
+
+}
---
+++ AddressBook/src/test/java/com/spring_address_book/AddressBook/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_address_book.AddressBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
